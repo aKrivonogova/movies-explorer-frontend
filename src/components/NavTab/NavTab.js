c2ffc58c7f1f3b1fac4e955016b8ca918d46c1dc
aKrivonogova <krivonogova.aline@yandex.ru>
2023-08-09 00:11:00 +0300

    4.Обернуть блоки в ссылки

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -2,9 +2,9 @@ import './NavTab.css'
 function NavTab(props) {
     return (
         <>
-            <li className="promo__link-item">
-                <a className="promo__link" href={props.linkPath}>
-                    {props.linkName}</a></li>
+            <a className="promo__link" href={props.linkPath}>
+                <li className="promo__link-item"> {props.linkName}</li >
+            </a>
         </>
     )
 }
